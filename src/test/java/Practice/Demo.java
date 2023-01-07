f2307a7f45e16ea0ee0626f2e976f8781c225454
AMARSRAVANI <AMARSRAVANI@LAPTOP-LBIUQGKJ>
2023-01-07 15:52:08 +0530

    added deme3 method to Demo class

--- src/test/java/Practice/Demo.java
+++ src/test/java/Practice/Demo.java
@@ -1,7 +1,10 @@
 package Practice;
 
+import org.testng.annotations.Test;
+
 public class Demo 
 {
+	@Test
 	public void demo1()
 	{
 		System.out.println("demo1");
@@ -10,6 +13,11 @@ public class Demo
 	public void demo2()
 	{
 		System.out.println("demo2");
+	}
+	
+	public void demo3()
+	{
+		System.out.println("demo3");
 	}
 
 }
